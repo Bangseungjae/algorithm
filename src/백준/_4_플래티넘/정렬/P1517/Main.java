8b7454662fdca5396d1b249e0be3d96e00721db7
Bangseungjae <tmdwo5825@naver.com>
2024-01-25 05:22:53 +0900

    backjoon 1517 platinum5

---
+++ src/백준/_4_플래티넘/정렬/P1517/Main.java
@@ -0,0 +1,63 @@
+package 백준._4_플래티넘.정렬.P1517;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] A, tmp;
+    public static long result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        A = new int[N + 1];
+        tmp = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        result = 0;
+        mergeSort(1, N); //병합정렬 수행하기
+        System.out.println(result);
+    }
+
+    private static void mergeSort(int s, int e) {
+        if (e - s < 1)
+            return;
+
+        int m = s + (e - s) / 2;
+        mergeSort(s, m);
+        mergeSort(m + 1, e);
+
+        for (int i = s; i <= e; i++) {
+            tmp[i] = A[i];
+        }
+        int k = s;
+        int index1 = s;
+        int index2 = m + 1;
+        while (index1 <= m && index2 <= e) {
+            if (tmp[index1] > tmp[index2]) {
+                A[k] = tmp[index2];
+                result += index2 - k;
+                k++;
+                index2++;
+            } else {
+                A[k] = tmp[index1];
+                k++;
+                index1++;
+            }
+        }
+        while (index1 <= m) {
+            A[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+        while (index2 <= e) {
+            A[k] = tmp[index2];
+            k++;
+            index2++;
+        }
+    }
+}
