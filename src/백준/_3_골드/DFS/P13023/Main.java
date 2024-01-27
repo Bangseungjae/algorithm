9a56612c9f73570c6667fc8294f01eff81b28f63
Bangseungjae <tmdwo5825@naver.com>
2024-01-28 02:51:03 +0900

    baekjoon P13023 gold5 dfs

---
+++ src/백준/_3_골드/DFS/P13023/Main.java
@@ -0,0 +1,59 @@
+package 백준._3_골드.DFS.P13023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] isVisit;
+    static ArrayList<Integer>[] A;
+    static boolean alive = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        isVisit = new boolean[N];
+        A = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < M + 1; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            A[from].add(to);
+            A[to].add(from);
+        }
+        for (int i = 0; i < N; i++) {
+            dfs(i, 1);
+        }
+        if (alive)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+
+    static void dfs(int index, int depth) {
+        if (depth == 5 || alive) {
+            alive = true;
+            return;
+        }
+        if (isVisit[index]) {
+            return;
+        }
+        isVisit[index] = true;
+
+        for (int to : A[index]) {
+            if (!isVisit[to]) {
+                dfs(to, depth + 1);
+            }
+        }
+
+        isVisit[index] = false;
+    }
+}
