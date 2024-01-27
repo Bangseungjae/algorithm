5a3ef8564920acefc1dc9a2b9a7aab79c632bb5e
Bangseungjae <tmdwo5825@naver.com>
2024-01-28 01:15:12 +0900

    baekjoon P2023 gold5 dfs

---
+++ src/백준/_2_실버/DFS/P2023/Main.java
@@ -0,0 +1,44 @@
+package 백준._2_실버.DFS.P2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+    }
+
+    static void dfs(int num, int jarisu) {
+        if (jarisu == N) {
+            if (isPrime(num)) {
+                System.out.println(num);
+            }
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(num * 10 + i))
+                dfs(num * 10 + i, jarisu + 1);
+        }
+    }
+
+    static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
