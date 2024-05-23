f55adf1edf4bbc0e05eea3c92d53bac14f5fc99f
Bangseungjae <tmdwo5825@naver.com>
2024-05-24 00:28:16 +0900

    프로그래머스 미로탈출 lv2

---
+++ src/프로그래머스/lv2/미로탈출/Main.kt
@@ -0,0 +1,58 @@
+package 프로그래머스.lv2.미로탈출
+
+import java.util.*
+
+class Solution {
+    val a = arrayOf(0, 0, 1, -1)
+    val b = arrayOf(1, -1, 0, 0)
+    var leverPair = Pair(0, 0)
+    var exitPair = Pair(0, 0)
+    var startPair = Pair(0, 0)
+    fun solution(maps: Array<String>): Int {
+        repeat(maps.size) { x ->
+            repeat(maps[0].length) { y ->
+                if (maps[x][y] == 'S') {
+                    startPair = Pair(x, y)
+                } else if (maps[x][y] == 'L') {
+                    leverPair = Pair(x, y)
+                } else if (maps[x][y] == 'E') {
+                    exitPair = Pair(x, y)
+                }
+            }
+        }
+        fun dfs(start: Pair<Int, Int>, end: Pair<Int, Int>, map :Array<String>): Int {
+            val visited = Array(map.size) {IntArray(map[0].length) {-1}}
+            visited[start.first][start.second] = 0
+            val queue: Queue<Pair<Int, Int>> = LinkedList()
+            queue.add(start.first to start.second)
+            while (queue.isNotEmpty()) {
+                val (dX, dY) = queue.poll()
+                if (dX == end.first && dY == end.second) {
+                    return visited[dX][dY]
+                }
+                for (i in 0..3) {
+                    val pointX = dX + a[i]
+                    val pointY = dY + b[i]
+                    if (pointX in maps.indices &&
+                        pointY in maps[0].indices &&
+                        visited[pointX][pointY] == -1 &&
+                        maps[pointX][pointY] != 'X'
+                    ) {
+                        visited[pointX][pointY] = visited[dX][dY] + 1
+                        queue.add(pointX to pointY)
+                    }
+                }
+            }
+
+            return -1
+        }
+
+        val lever = dfs(startPair, leverPair, maps)
+        val exit = dfs(leverPair, exitPair, maps)
+        return if (lever != -1 && exit != -1) {
+            lever + exit
+        } else {
+            -1
+        }
+    }
+}
